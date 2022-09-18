23b5fd6d320112577ad46628758020979d821850
Magnus <Magnusvjensen@gmail.com>
2022-09-18 11:19:20 +0200

    Adding scrollview

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from "react";
 import {
   StyleSheet,
   Text,
-  Button,
   Pressable,
   Image,
   StatusBar,
   TextInput,
   View,
   Alert,
+  ImageBackground,
+  ScrollView,
 } from "react-native";
 
 export default function App() {
@@ -21,34 +22,59 @@ export default function App() {
         <TextInput
           style={styles.input}
           onChangeText={onChangeText}
+          placeholder={text}
           value={text}
         />
       </View>
-      <View style={styles.section_one}>
-        <Image
-          style={styles.image}
-          source={{
-            uri: "https://picsum.photos/200",
-          }}
-        />
-        <Pressable
-          style={styles.button}
-          title="Learn More"
-          onPress={() => Alert.alert("Simple Button pressed")}
-        >
-          <Text>Click Me</Text>
-        </Pressable>
-      </View>
+      <ScrollView style={styles.scrollView} horizontal>
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+          <View key={i} style={styles.section_one}>
+            <Image
+              style={styles.image}
+              source={{
+                uri: `https://picsum.photos/${i}00`,
+              }}
+            />
+            <Pressable
+              style={styles.button}
+              title="Learn More"
+              onPress={() => Alert.alert("Simple Button pressed")}
+            >
+              <Text>Click Me</Text>
+            </Pressable>
+          </View>
+        ))}
+      </ScrollView>
       <View style={styles.section_two}>
-        <Image
-          style={styles.image}
+        <ImageBackground
+          style={styles.imageBackground}
           source={{
-            uri: "https://picsum.photos/300",
+            uri: "https://picsum.photos/900",
           }}
-        />
+        >
+          <Text
+            style={{
+              color: "white",
+              fontWeight: "900",
+              textShadowColor: "rgba(0, 0, 0, 0.75)",
+              textShadowOffset: { width: -1, height: 1 },
+              textShadowRadius: 10,
+              fontSize: 22,
+            }}
+          >
+            I am text on top of image
+          </Text>
+          <Pressable
+            style={styles.button}
+            title="Learn More"
+            onPress={() => Alert.alert("Simple Button pressed")}
+          >
+            <Text>Button on BG image</Text>
+          </Pressable>
+        </ImageBackground>
       </View>
       <View style={styles.footer}>
-        <TextInput style={styles.input} value={"TextInput"} />
+        <Text style={styles.footer.text}>{text !== "Input name" && text}</Text>
       </View>
     </View>
   );
@@ -65,7 +91,7 @@ const styles = StyleSheet.create({
     padding: 3,
     paddingHorizontal: 20,
     paddingVertical: 10,
-    borderRadius: 5
+    borderRadius: 5,
   },
   header: {
     borderWidth: 1,
@@ -80,9 +106,14 @@ const styles = StyleSheet.create({
   input: {
     height: 60,
     borderWidth: 1,
-    borderColor: "red",
-    padding: 6,
+    borderColor: "grey",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
     width: "75%",
+    borderRadius: 4,
+  },
+  scrollView: {
+    height: 50,
   },
   section_one: {
     flex: 1,
@@ -90,6 +121,7 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
+    marginHorizontal: 5,
   },
   section_two: {
     flex: 1,
@@ -102,13 +134,27 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
   },
+  imageBackground: {
+    width: "100%",
+    height: "100%",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   footer: {
     height: 60,
-    borderWidth: 1,
-    backgroundColor: "white",
+    backgroundColor: "#500052",
     padding: 6,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
+    text: {
+      color: "white",
+      textShadowColor: "rgba(0, 0, 0, 0.75)",
+      textShadowOffset: { width: -1, height: 1 },
+      textShadowRadius: 10,
+      fontSize: 22,
+      fontFamily: "Roboto",
+    },
   },
 });
